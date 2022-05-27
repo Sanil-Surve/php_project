51750de4c6da8366bac0c519a8c0f7055ed50fa3
Sanil-Surve <sanilsurve42@gmail.com>
2022-05-27 22:05:45 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Application</title>
+</head>
+<body>
+    <form action="insert.php" method="POST">
+        FirstName: <input type="text" name="fname" /> <br> <br>
+        LastName: <input type="text" name="lname" /> <br> <br>
+        <input type="submit" value="Submit" />
+</body>
+</html>
